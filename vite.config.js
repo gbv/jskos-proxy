e6e318d6523cbb7759ed1209f2ded45f37de4cbb
Stefan Peters <Stefan.Peters@gbv.de>
2023-05-17 15:43:26 +0200

    Vite: Add source maps for non-production builds

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    // Build source maps for non-production builds
+    sourcemap: process.env.NODE_ENV === "production" ? false : true,
+  },
 })
