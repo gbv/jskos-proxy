c6b7aad1827901a6dd055796039b0b2c752148c6
Stefan Peters <Stefan.Peters@gbv.de>
2024-05-17 13:13:28 +0200

    HomeView: Add different modes

--- src/client/views/HomeView.vue
+++ src/client/views/HomeView.vue
@@ -1,56 +1,96 @@
 <script setup>
-import { RouterLink } from "vue-router"
+import { useRoute } from "vue-router"
 import config from "@/config.js"
 import * as jskos from "jskos-tools"
 import { schemes, quickSelection, publisherSelection, typeSelection } from "@/store.js"
+import { computed } from "vue"
+
+const route = useRoute()
+
+const mode = computed(() => {
+  if (route.query?.publisher) {
+    return "publisher"
+  }
+  if (route.query?.type) {
+    return "type"
+  }
+  if (route.query?.search) {
+    return "search"
+  }
+  if (route.query?.conceptSearch) {
+    return "conceptSearch"
+  }
+  return "default"
+})
 
 </script>
 
 <template>
-  <main v-if="schemes">
-    <div 
-      v-if="quickSelection.length"
-      class="section">
-      <h2>Schnellzugriff</h2>
-      <div class="quickSelection">
-        <RouterLink
-          v-for="scheme in quickSelection"
-          :key="scheme.uri"
-          :to="`${config.namespace.pathname}${scheme.uri.replace(config.namespace, '')}`">
-          {{ jskos.prefLabel(scheme) }}
-        </RouterLink>
-      </div>
-    </div>
+  {{ mode }}
+  <template v-if="schemes">
+    <!-- Default view: show quick view, publisher selection, type selectio -->
     <div
-      v-if="publisherSelection.length"
-      class="section">
-      <h2>Herausgeber</h2>
-      <div class="publisherSelection">
-        <RouterLink
-          v-for="publisher in publisherSelection"
-          :key="publisher"
-          :to="`?publisher=${encodeURIComponent(publisher)}`">
-          {{ publisher }}
-        </RouterLink>
+      v-if="mode === 'default'">
+      <div 
+        v-if="quickSelection.length"
+        class="section">
+        <h2>Schnellzugriff</h2>
+        <div class="quickSelection">
+          <RouterLink
+            v-for="scheme in quickSelection"
+            :key="scheme.uri"
+            :to="`${config.namespace.pathname}${scheme.uri.replace(config.namespace, '')}`">
+            {{ jskos.prefLabel(scheme) }}
+          </RouterLink>
+        </div>
+      </div>
+      <div
+        v-if="publisherSelection.length"
+        class="section">
+        <h2>Herausgeber</h2>
+        <div class="publisherSelection">
+          <RouterLink
+            v-for="publisher in publisherSelection"
+            :key="publisher"
+            :to="`?publisher=${encodeURIComponent(publisher)}`">
+            {{ publisher }}
+          </RouterLink>
+        </div>
+      </div>
+      <div
+        v-if="typeSelection.length"
+        class="section">
+        <h2>Vokabulartyp</h2>
+        <div class="typeSelection">
+          <RouterLink
+            v-for="t in typeSelection"
+            :key="t"
+            :to="`?type=${encodeURIComponent(t.uri)}`">
+            {{ jskos.prefLabel(t) }}
+          </RouterLink>
+        </div>
       </div>
     </div>
-    <div
-      v-if="typeSelection.length"
-      class="section">
-      <h2>Vokabulartyp</h2>
-      <div class="typeSelection">
-        <RouterLink
-          v-for="type in typeSelection"
-          :key="type"
-          :to="`?type=${encodeURIComponent(type.uri)}`">
-          {{ jskos.prefLabel(type) }}
-        </RouterLink>
-      </div>
+    <!-- Publisher view: show schemes for particular publisher -->
+    <div v-if="mode === 'publisher'">
+      TODO - Publisher view: {{ route.query.publisher }}
     </div>
-  </main>
-  <main v-else>
+    <!-- Type view: show schemes for particular type -->
+    <div v-if="mode === 'type'">
+      TODO - Type view: {{ route.query.type }}
+    </div>
+    <!-- Search view: show scheme search results -->
+    <div v-if="mode === 'search'">
+      TODO - Search view: {{ route.query.search }}
+    </div>
+    <!-- Concept search view: show concept search results -->
+    <div v-if="mode === 'searchConcept'">
+      TODO - Concept search view: {{ route.query.searchConcept }}
+    </div>
+  </template>
+  <div v-else>
     <LoadingIndicator />
-  </main>
+  </div>
 </template>
 
 <style scoped>
