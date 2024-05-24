9bb75a9d5121b51d4f02b9147e1ec5dc05ec905d
Stefan Peters <Stefan.Peters@gbv.de>
2024-05-24 10:48:48 +0200

    Header: Small adjustment to search

--- src/client/components/TheHeader.vue
+++ src/client/components/TheHeader.vue
@@ -1,15 +1,21 @@
 <script setup>
 import { inject, ref, watch } from "vue"
-import { useRouter } from "vue-router"
+import { useRouter, useRoute } from "vue-router"
 
 const config = inject("config")
 const router = useRouter()
+const route = useRoute()
 
 // TODO: Improve search (probably use API instead)
 const search = ref("")
 watch(search, (value) => {
-  router.push(`${config.namespace.pathname}?search=${encodeURIComponent(value)}`)
+  router.push(`${config.namespace.pathname}${value === "" ? "" : "?search="}${encodeURIComponent(value)}`)
 })
+watch(() => route.query.search, (value) => {
+  if (value !== search.value) {
+    search.value = value || ""
+  }
+}, { immediate: true })
 </script>
 
 <template>
