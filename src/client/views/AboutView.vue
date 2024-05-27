9f81a006e3a5db3e33df4e78479146512ec83740
Stefan Peters <stefandesu@exo.pm>
2024-05-27 11:50:29 +0200

    Add About text

--- src/client/views/AboutView.vue
+++ src/client/views/AboutView.vue
@@ -1,6 +1,37 @@
+<script setup>
+import config from "@/config.js"
+import { locale } from "@/store.js"
+</script>
+
 <template>
-  <div class="section">
+  <!-- TODO: Styles -->
+  <div
+    class="section"
+    style="max-width: 1200px; margin: 0 auto;">
     <h1>{{ $t("aboutTitle") }}</h1>
+    <template v-if="locale === 'de'">
+      <p>
+        Unter <a :href="config.namespace.pathname">{{ config.namespace }}</a>
+        können <b>kontrollierte Vokabulare</b> (Normdateien, Klassifikationen, Thesauri...)
+        in einer Webansicht und als Linked Open Data abgerufen werden.
+      </p>
+      <p>
+        Die Daten stammen von <code>{{ config.backend }}</code> und werden mittels
+        <a :href="config.homepage">{{ config.name }}</a> umgesetzt.
+      </p>
+    </template>
+    <template v-else>
+      <!-- TODO -->
+      <p>
+        <a :href="config.namespace.pathname">{{ config.namespace }}</a>
+        allows <b>controlled vocabularies</b> (norm data, classifications, thesauri...)
+        to be browsed in a web view and accessed as Linked Open Data.
+      </p>
+      <p>
+        Data is accessed from <code>{{ config.backend }}</code> and implemented via
+        <a :href="config.homepage">{{ config.name }}</a>.
+      </p>
+    </template>
   </div>
 </template>
 
