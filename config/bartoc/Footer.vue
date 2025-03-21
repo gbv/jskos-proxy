cb6bcb14d94d2578959e05219dfe40fae417ebe0
Monty <Monty.Bitto@gbv.de>
2025-03-21 10:44:03 +0100

    New bartoc config

---
+++ config/bartoc/Footer.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div style="flex: 1;">
+    BARTOC.org
+  </div>
+  <div>
+    <a href="https://www.gbv.de/impressum">Impressum</a><br>
+    <a href="https://www.gbv.de/datenschutz">Datenschutz</a><br>
+    <a href="https://bartoc.org/contact">Kontakt</a>
+  </div>
+</template>
